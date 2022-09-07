f370e829b49933b2833f8c705bfd631f33ed5030
Anastasiia Trubchaninova <anastasiia.trubchaninova@student.put.poznan.pl>
2022-09-07 12:34:11 +0200

    create scenario 2

--- cypress/e2e/automation-chalange/tests-scenarios.cy.js
+++ cypress/e2e/automation-chalange/tests-scenarios.cy.js
@@ -1,5 +1,5 @@
 describe("Automatio-chalange", function () {
-  before(function () {
+  beforeEach(function () {
     cy.visit("https://www.danfoss.com/en/");
     cy.contains("Accept all").click();
   });
@@ -7,10 +7,14 @@ describe("Automatio-chalange", function () {
   it("Scenario 1 - visible button with a text", function () {
     cy.contains("Explore Danfoss Product Store").should("be.visible");
   });
+
   it("Scenario 2 - Alsense search results are available", function () {
-    cy.contains("Alsense IoT cloud monitoring").should("be.visible");
-    cy.get("a").invoke("href").should(contain.text, ".pdf");
+    cy.get('input[type="text"]').type("Alsense");
+    cy.get("body").type("{enter}");
+    cy.contains("Alsense IoT cloud and monitoring").should("be.visible");
+    cy.get("a[href$='.pdf'].tile__link").should("exist");
   });
+
   it("Scenario 3 - Quick Links not visible in mobile view", function () {
     cy.viewport("samsung-note9");
     cy.contains("Quick Links").should("not.exist");
