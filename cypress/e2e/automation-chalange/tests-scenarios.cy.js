f13ad88bc248088e42094c53bc8e427160fcca8b
Anastasiia Trubchaninova <anastasiia.trubchaninova@student.put.poznan.pl>
2022-09-07 11:22:45 +0200

    create scenario 3

--- cypress/e2e/automation-chalange/tests-scenarios.cy.js
+++ cypress/e2e/automation-chalange/tests-scenarios.cy.js
@@ -7,4 +7,12 @@ describe("Automatio-chalange", function () {
   it("Scenario 1 - visible button with a text", function () {
     cy.contains("Explore Danfoss Product Store").should("be.visible");
   });
+  it("Scenario 2 - Alsense search results are available", function () {
+    cy.contains("Alsense IoT cloud monitoring").should("be.visible");
+    cy.get("a").invoke("href").should(contain.text, ".pdf");
+  });
+  it("Scenario 3 - Quick Links not visible in mobile view", function () {
+    cy.viewport("samsung-note9");
+    cy.contains("Quick Links").should("not.exist");
+  });
 });
